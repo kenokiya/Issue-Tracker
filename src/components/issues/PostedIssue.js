a75e9112e74434757a18ac24c6e6f8cff6d95ece
pekstaar <pekstaar@gmail.com>
2022-03-04 14:46:52 +0300

    UPDATE:

--- src/components/issues/PostedIssue.js
+++ src/components/issues/PostedIssue.js
@@ -8,41 +8,41 @@ import { Link } from "react-router-dom";
 
 export const PostedIssue = () => {
   return (
-    <Link to="id">
-      <Wrapper className="flex flex-col justify-between gap-3 my-2">
-        {/* nav */}
-        <div className="flex justify-between items-center">
-          {/* rounded persona*/}
-          <div className="h-6 w-6 rounded-full bg-yellow"></div>
+    <Wrapper className="flex flex-col justify-between gap-3 my-2">
+      {/* nav */}
+      <div className="flex justify-between items-center">
+        {/* rounded persona*/}
+        <div className="h-6 w-6 rounded-full bg-yellow"></div>
 
-          {/* poster name */}
-          <Bold className="text-md">Robert Mwalugha</Bold>
-          {/* date */}
-          <SmallGrayText>Feb 23 2022</SmallGrayText>
-          {/* bin */}
-          <button>
-            <IoTrashOutline className="text-red-500" />
-          </button>
-        </div>
-        {/* description */}
-        <p className=" text-gray-700 px-1" style={{ fontSize: "13px" }}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
-          quas alias, voluptatum modi incidunt distinctio culpa laudantium enim
-          odit ipsam.
-          <Bold className="text-sm text-black ml-2">@PeterM</Bold>
-        </p>
-        {/* images section */}
-        <div className="flex gap-2">
-          {/* image 1 */}
-          <div className="h-20 w-24 bg-slate-400 rounded"></div>
-          <div className="h-20 w-24 bg-slate-400 rounded"></div>
-        </div>
-        {/* actions */}
-        <div className="flex justify-end gap-2">
+        {/* poster name */}
+        <Bold className="text-md">Robert Mwalugha</Bold>
+        {/* date */}
+        <SmallGrayText>Feb 23 2022</SmallGrayText>
+        {/* bin */}
+        <button>
+          <IoTrashOutline className="text-red-500" />
+        </button>
+      </div>
+      {/* description */}
+      <p className=" text-gray-700 px-1" style={{ fontSize: "13px" }}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati quas
+        alias, voluptatum modi incidunt distinctio culpa laudantium enim odit
+        ipsam.
+        <Bold className="text-sm text-black ml-2">@PeterM</Bold>
+      </p>
+      {/* images section */}
+      <div className="flex gap-2">
+        {/* image 1 */}
+        <div className="h-20 w-24 bg-slate-400 rounded"></div>
+        <div className="h-20 w-24 bg-slate-400 rounded"></div>
+      </div>
+      {/* actions */}
+      <div className="flex justify-end gap-2">
+        <Link to="id">
           <EditButton>Edit</EditButton>
-          <CompleteButton>complete</CompleteButton>
-        </div>
-      </Wrapper>
-    </Link>
+        </Link>
+        <CompleteButton>complete</CompleteButton>
+      </div>
+    </Wrapper>
   );
 };
---
+++ src/components/issues/PostedIssueReply.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+/**
+ *
+ * @returns reply from tagged user or any other user.
+ */
+
+export const PostedIssueReply = () => {
+  return <div>PostedIssueReply</div>;
+};
